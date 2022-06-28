b01c574064caaad72be4daeebaa32dd79cf8eada
Paul Sheridan <psheridan@ITs-MacBook-Pro.local>
2022-06-28 14:07:07 -0500

    first commit

---
+++ employee-growth-platform/layers/logger.py
@@ -0,0 +1,11 @@
+from aws_lambda_powertools import Logger
+logger = Logger()
+
+"""Log info messages"""
+def info(log_message):
+    logger.info(log_message)
+
+
+"""Log error messages"""
+def error(log_message):
+    logger.error(log_message)
